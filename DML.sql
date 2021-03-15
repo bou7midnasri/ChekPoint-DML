51eff67d0a30153f4f19c83de00966c0f464ae71
Ahmed <bouhmidnasri88@gmail.com>
2021-03-15 21:14:42 +0100

    Data Manipulation Language

---
+++ DML.sql
@@ -0,0 +1,11 @@
+/* insertion dans table product */
+Insert into product Values('P01','Samsung Galaxy 520','Smartphone',3299);
+Insert into product Values('P02','Assus Netbook','pc',4599);
+
+/*insertion dans la table customer*/
+Insert into customer Values('C01','Ali',71321009);
+Insert into customer Values('C01','Asma',77345823);
+
+/*insertion dans la table orders*/
+Insert into orders Values('C01','P02',2 ,9198);
+Insert into orders Values('C02','P01',28/05/2020,1 ,3299;
